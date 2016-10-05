1b5299efa0e94042f2c7c7cbac762b29371b4148
jinnudeveloper <jinnudeveloper@gmail.com>
2016-10-05 15:48:56 +0530

    Prgm Modified

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -4,8 +4,8 @@ end
 
 class Hamming
   def self.compute(strand1, strand2)
-    strand1.length == strand2.length ||
-      fail(ArgumentError, 'The two strands must have the same length.')
+  fail(ArgumentError, 'The two strands must have the same length.') if   strand1.length != strand2.length
+#raise ArgumentError if strand1.length != strand2.length
     (0...strand1.length).count do |i|
       strand1[i] != strand2[i]
     end
