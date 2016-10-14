3752ec8fff20036ed133a80573a3ac3329a9fbe6
jinnudeveloper <jinnudeveloper@gmail.com>
2016-10-14 10:40:18 +0530

    Old Raindrop version

---
+++ ruby/raindrops/raindrops1.rb
@@ -0,0 +1,32 @@
+module BookKeeping
+  VERSION = 3
+end
+
+class Raindrops
+
+  def self.convert(number)
+    a = []
+    if number == 1
+      return number.to_s
+    else
+
+      (2..number).each do|i|
+        if number % i == 0
+          a << i
+        end
+      end
+      raindrops = ""
+      raindrops << "Pling" if a.include?(3)
+      raindrops << "Plang" if a.include?(5)
+      raindrops << "Plong" if a.include?(7)
+
+      raindrops.empty? ? number.to_s : raindrops
+    end
+  end
+end
+=begin
+   phrases = ""
+   phrases << 'Pling' if number.modulo(3).zero?
+   phrases << 'Plang' if number.modulo(5).zero?
+   phrases << 'Plong' if number.modulo(7).zero?
+end
