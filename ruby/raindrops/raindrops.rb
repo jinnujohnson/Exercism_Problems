71b7092ecf685bd8defb02e49b6ce18d4281358d
jinnudeveloper <jinnudeveloper@gmail.com>
2016-10-14 10:40:38 +0530

    Modified Pgm

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -5,22 +5,11 @@ end
 class Raindrops
 
   def self.convert(number)
-    a = []
-    if number == 1
-      return number.to_s
-    else
-
-      (2..number).each do|i|
-        if number % i == 0
-          a << i
-        end
-      end
       raindrops = ""
-      raindrops << "Pling" if a.include?(3)
-      raindrops << "Plang" if a.include?(5)
-      raindrops << "Plong" if a.include?(7)
+      raindrops << "Pling" if number % 3 == 0
+      raindrops << "Plang" if number % 5 == 0
+      raindrops << "Plong" if number % 7 == 0
 
       raindrops.empty? ? number.to_s : raindrops
     end
   end
-end
