f10f946193e36047ba807bdaf4f1d71315ba527a
jinnudeveloper <jinnudeveloper@gmail.com>
2016-10-16 07:47:20 +0530

    Replace variable count to limit and use n

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -3,16 +3,17 @@ module BookKeeping
 end
 
 class Squares
-  def initialize(count)
-    @count = count
+
+  def initialize(limit)
+    @n = limit
   end
 
   def square_of_sum
-    (@count * (@count + 1) / 2) ** 2
+    (@n * (@n + 1) / 2) ** 2
   end
 
   def sum_of_squares
-    (@count * (@count + 1) * (2 * @count + 1)) / 6
+    (@n * (@n + 1) * (2 * @n + 1)) / 6
   end
 
   def difference
