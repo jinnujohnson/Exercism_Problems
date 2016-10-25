a972351a51c2d92d841f56a39f0a0e41f9523d21
jinnudeveloper <jinnudeveloper@gmail.com>
2016-10-25 21:43:28 +0530

    Robot name pgm

---
+++ ruby/robot-name/robot_name.rb
@@ -0,0 +1,43 @@
+module BookKeeping
+  VERSION = 2
+end
+
+class Robot
+  @taken_names = {}
+
+  def name
+    @name ||= generate_name
+  end
+
+  def reset
+    @name = nil
+  end
+
+  def self.taken_names
+    @taken_names
+  end
+
+  private
+
+  def generate_name
+    name = "#{prefix}#{suffix}"
+    if self.class.taken_names[name]
+      generate_name
+    else
+      self.class.taken_names[name] = true
+      name
+    end
+  end
+
+  def prefix
+    alphabet.sample(2).join('')
+  end
+
+  def suffix
+    rand(100...999)
+  end
+
+  def alphabet
+    ('A'..'Z').to_a
+  end
+end
