48c7512bbcdf7e441f0574357fe0b908bc6d5760
jinnudeveloper <jinnudeveloper@gmail.com>
2016-10-27 19:17:41 +0530

    Modified variable names

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,8 +1,9 @@
 class Grains
-  def self.square(squares)
-    return 2**(squares-1)
+  def self.square(nthsquare)
+    2**(nthsquare - 1)
   end
   def self.total
-    return ( (1 - 2**64) / (1-2) )
+    squares = 64
+    (1 - 2**squares) / (1-2)
   end
 end
