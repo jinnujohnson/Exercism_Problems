7643ba8eb32e728b124bcc9f377ec118b914ad6d
jinnudeveloper <jinnudeveloper@gmail.com>
2016-11-18 13:25:07 +0530

    Done Program

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -1,11 +1,12 @@
 class Array
   def keep
     each_with_object([]) do |obj, array|
-      array << yield(obj)
+      array << obj if yield(obj) == true
     end
+  end
     def discard
       each_with_object([]) do |obj, array|
-        array << yield(obj)
+        array << obj if yield(obj) == false
       end
+    end
   end
-end
