84d87fb250649a6e0eb0b46e8b041aad69701408
jinnudeveloper <jinnudeveloper@gmail.com>
2016-11-24 16:47:48 +0530

    Program finished

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -3,12 +3,19 @@ class RunLengthEncoding
   def self.encode(string)
     a =[]
     string.each_char.inject([]) do |memo, c|
-     a.last && c == a.last.last ? a.last[0] += 1 : a << [1,c]
+      a.last && c == a.last.last ? a.last[0] += 1 : a << [1,c]
       a
-   end
-   a.flatten!
-   a  -= [1]
-   a.join
- end
+    end
+    a.flatten!
+    a  -= [1]
+    a.join
+  end
+
+  def self.decode(string)
+    string.scan(/(\d*)(\D)/).collect do |length, char|
+      length = 1 if length.empty?
+      char * length.to_i
+    end.join
+  end
 
 end
