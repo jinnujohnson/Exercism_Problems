f0b09b2d4bd52d8d3ea7beb59dbb3b1b22268094
jinnudeveloper <jinnudeveloper@gmail.com>
2016-11-24 15:57:56 +0530

    Encoding module finished

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,18 +1,14 @@
 class RunLengthEncoding
-  def self.encode(string)
-    string.chars.map do |char, array|
-      if array.length > 1
-        [ array.length, char]
-      else
-        ['', char]
-      end
-    end.join
-  end
 
-  def self.decode(string)
-    string.scan(/(\d*)(\D)/).collect do |length, char|
-      length = 1 if length.empty?
-      char * length.to_i
-    end.join
-  end
+  def self.encode(string)
+    a =[]
+    string.each_char.inject([]) do |memo, c|
+     a.last && c == a.last.last ? a.last[0] += 1 : a << [1,c]
+      a
+   end
+   a.flatten!
+   a  -= [1]
+   a.join
+ end
+
 end
